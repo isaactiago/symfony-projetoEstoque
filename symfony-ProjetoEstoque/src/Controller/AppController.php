3226960a5771ab1bd87686726be4a416e37432ed
isaac <isaactiagowandermure.com>
2024-09-17 23:29:42 -0300

    first commit

---
+++ symfony-ProjetoEstoque/src/Controller/AppController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route("/app", name:'app', methods:'GET')]
+class AppController extends AbstractController
+{
+    public function __invoke(Request $request): Response
+    {
+
+        return $this->render('app/index.html.twig');
+    }
+}
+
+?>
