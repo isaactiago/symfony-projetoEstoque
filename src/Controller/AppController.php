0ba79188c6ec8409e79561c041da07e331ee05de
isaac <isaactiagowandermure.com>
2024-09-19 19:58:27 -0300

    parte de login e registar usuario pronto

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
